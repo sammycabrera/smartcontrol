dc614b49df5c504ae763f71f12c2f82341072995
sammycabrera <sammy_cabrera@hotmail.com>
2024-04-09 23:08:56 -0500

    publicar

--- components/rtgraph/fetchdatagh.js
+++ components/rtgraph/fetchdatagh.js
@@ -2,10 +2,11 @@ export async function getDataSimpleChart(preview) {
   try {
     const response = await fetch(
       process.env.NEXT_PUBLIC_API_URL + "api/dot/2/"
-    ); // Replace with your API endpoint
+    ); // Replace with your API end point
     if (!response.ok) {
       throw new Error("Network response was not ok.");
     }
+
     const data = await response.json();
     console.log(data);
     return data;
